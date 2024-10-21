ed74ebf1f4539b3d6e9357cc64c55439801dd7f0
Hilary-Collado <hilarycollado1@gmail.com>
2024-10-20 21:45:33 -0400

    agregando mapa para resaltar ubicacion del restaurante

--- src/Components/Principal/Table/Table.jsx
+++ src/Components/Principal/Table/Table.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import './_Table.sass';
-import BookTable from '../../Img/wine.jpg'
+import BookTable from '../../Img/restaurant.jpg'
+import Map from '../../Img/Map.jpg'
 
 function Table() {
     return (
         <>
             <section className="table" id="Table">
-                <div className="container">
-                    <img src={BookTable} className="bookTable" alt="" />
+                {/* <div className="contenido">
+                    <img src={Map} className="map" alt="" /> */}
 
-                    <div className="overlay">
-                        <h2 className="h2">Book your table</h2>
-                        <button className="button">online booking</button>
+                    <div className="container">
+                        <img src={BookTable} className="bookTable" alt="" />
+
+                        <div className="overlay">
+                            <h2 className="h2">Book your table</h2>
+                            <button className="button">online booking</button>
+                        </div>
                     </div>
-                </div>
+                {/* </div> */}
             </section>
 
             {/* <div className="image-container">
